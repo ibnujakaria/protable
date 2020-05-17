dfff15785332230194949f2b068b1d8b051c5aa5
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-05-17 14:35:28 +0700

    add babel-loader and @babel/plugin-proposal-optional-chaining

--- webpack.common.js
+++ webpack.common.js
@@ -27,6 +27,17 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ["@babel/plugin-proposal-optional-chaining"]
+          }
+        }
+      },
+      {
         test: /\.scss$/,
         use: [
           {
